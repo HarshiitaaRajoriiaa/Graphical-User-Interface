5106bd2894d9f2233ed9d0a202636cbd2845ed66
Harshita Rajoria <harshitarajoria02@gmail.com>
2024-06-23 17:28:36 +0530

    commit

--- Labels.java
+++ Labels.java
@@ -1,13 +1,18 @@
 // * JLabel is used to display a short string or an image icon. JLabel can display text, image or both
+import javax.swing.*;
 
-import javax.swing.JFrame;
 public class Labels {
     public static void main(String[] args) {
-         JFrame frame = new JFrame(); 
+        JFrame frame = new JFrame(); 
         frame.setTitle("Labels");
         frame.setVisible(true); 
         frame.setSize(400,400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel label = new JLabel();
+        label.setText("Label01");
+        
+
 
     }
 }
