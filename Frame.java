3b39a48e13a049b28c126a57e148c58c0713b86a
Harshita Rajoria <harshitarajoria02@gmail.com>
2024-06-23 16:19:37 +0530

    commit

--- Frame.java
+++ Frame.java
@@ -5,8 +5,8 @@ import javax.swing.JFrame;
 // * It can have other components like button, text field, scrollbar etc. Frame is most widely used container while developing an AWT application.
 public class Frame {
     public static void main(String[] args) {
-        
-        JFrame frame = new JFrame(); //instance of frame class
+        // * USING OBJECT INSTANCE FOR MAKING FRAMES:
+        JFrame frame = new JFrame(); //instance of this class
         frame.setVisible(true); //setVisible() to make frame visible
         frame.setSize(400,400); //setSize() to set size
         frame.setTitle("my first frame");
@@ -15,12 +15,11 @@ public class Frame {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit the frame on close
         frame.setResizable(false); //prevent from being resize manually with cursor
 
-
         // * setting custom image on the place of normal java logo:
         ImageIcon image = new ImageIcon("frame.jpg");
         frame.setIconImage(image.getImage());
 
-        // * setting background colr:
+        // * setting background color:
         frame.getContentPane().setBackground(Color.BLACK);
         frame.getContentPane().setBackground(Color.BLUE);
         //Range is 0 to 255
@@ -30,5 +29,26 @@ public class Frame {
         //using hexadecimal value of colors: can search on google for hexadecimal numbers of all the colors.
         frame.getContentPane().setBackground(new Color(0x123456)); // * dark blue
         frame.getContentPane().setBackground(new Color(0x126742)); //* green 
+
+        // * Calling constructor of Frame1 class:
+        Frame1 frame1 = new Frame1();
+        
     }
+}
+
+
+// * frame making as Inheritance:
+class Frame1 extends JFrame{
+    Frame1(){
+        this.setTitle("second frame");
+        this.setVisible(true);
+        this.setSize(500 ,500);
+        this.setResizable(false);
+        this.getContentPane().setBackground(new Color(0x121313));
+        ImageIcon image = new ImageIcon("frame.jpg");
+        this.setIconImage(image.getImage());
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+    }
+
 }
